81a433918107b21b0a65a59165673400ef44d00e
cbpolleydiscovr <christopher@discovranalytics.co>
2022-08-07 15:23:47 +0100

    add username

--- db/schema/createUsers.js
+++ db/schema/createUsers.js
@@ -9,8 +9,7 @@ exports.create = () => {
   CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(200) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    user_name VARCHAR (100) NOT NULL,
     password TEXT NOT NULL,
     token TEXT,
     shared_lists JSONB,
