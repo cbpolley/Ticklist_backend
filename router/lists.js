0e7a82597f9596bce96a620d994493ec699c0a05
cbpolleydiscovr <christopher@discovranalytics.co>
2022-08-31 22:06:08 +0100

    fix

--- router/lists.js
+++ router/lists.js
@@ -14,7 +14,7 @@ router.post('/add', listController.add);
 router.post('/edit', listController.edit);
 router.post('/sharedLists/add', listController.addToSharedLists);
 router.get('/single/userLists/:id', listController.getSingleUserSharedLists);
-router.get('/shareWithUsername', listController.shareWithUsername);
+router.post('/shareWithUsername', listController.shareWithUsername);
 router.delete('/delete/:id', listController.delete);
 
 module.exports = router
