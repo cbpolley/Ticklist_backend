55baf0ee65d4f8d365adaaf5567f57189cd50cc5
cbpolleydiscovr <christopher@discovranalytics.co>
2023-01-08 21:59:36 +0000

    fix

--- controllers/listController.js
+++ controllers/listController.js
@@ -68,15 +68,29 @@ exports.add = async (req, res, next) => {
 
   const share_uuid = req.body.packet.share_uuid;
   const list_name = req.body.packet.list.list_name;
+
+  const format_options = [
+    {id:1, isChecked:false, icon:'fa6-solid:list-ol', label: 'Numbered lists. Click on the numbered list icon to cycle through different list bullet points or numbers.'},
+    {id:2, isChecked:false, icon:'fluent:paint-bucket-16-filled', label: 'Colours. When active, click the paint bucket and you will be provided with a range of colours to choose from. You can give the whole Ticklist a colour, and you can colour individual items in the Ticklist.'},
+    {id:3, isChecked:false, icon:'charm:grab-horizontal', label: 'Allow list items to be moved. Do this by grabbing this icon and dragging the list item.'},
+    {id:4, isChecked:false, icon:'fluent:delete-12-regular', label: 'Delete mode. Click the delete icon to either delete the whole Ticklist or delete individual list items. Deleting is permanent.'},
+    {id:5, isChecked:false, icon:'carbon:progress-bar', label: 'Progress bar. Gives you a running completion percentage of your Ticklist.'},
+    {id:6, isChecked:false, icon:'ci:filter-outline', label: 'Filter between Ticklist items that are ticked and un-ticked.'},
+    {id:7, isChecked:false, icon:'bx:font-size', label: 'Change the size of the font for each Ticklist item.'},
+  ]
+
+  const json_format_options = JSON.stringify(format_options)
   const color = req.body.packet.list.color;
 
   const query = `
   INSERT INTO 
-    lists (list_name, share_uuid, color, list_contents, format_options, completed_percent, created_at, updated_at) 
+    lists (list_name, share_uuid, color, format_options, list_contents, completed_percent, created_at, updated_at) 
   VALUES 
-    ($1, $2, $3, "[]", "[]", 0, NOW(), NOW())`
+    ($1, $2, $3, $4, '"[]"', 0, NOW(), NOW())`
 
-    const values = [list_name, share_uuid, color]
+  console.log(query)
+
+    const values = [list_name, share_uuid, color, json_format_options]
 
   db
     .query(query, values)
